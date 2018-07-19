d321940591bc801f4e7c7c678c39afee8a126787
MikhailPixelStudios <mikhail.a.ushakov@gmail.com>
2018-07-19 15:10:42 +0300

    Comments added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 
 typedef unsigned char byte;
 using namespace std;
+
+//Класс Цвета
+//Class Color
+
 class Color{
 public:
     int r;
@@ -17,11 +21,27 @@ public:
     }
 };
 
+//Константы цветов
+//Color constants
+
+Color RED = Color(255,0,0);
+
+//Класс изображения
+//Image class
+
 class Image{
 public:
+
+//Ширина, высота, число каналов цвета
+//Width, height, number of channels in color
+
     int w;
     int h;
     int rgb = 3;
+
+//Данные изображения
+//Image data
+
     vector<byte> source;
     Image(int w,int h,int rgb){
         this->w=w;
@@ -30,12 +50,18 @@ public:
         this->source.resize(w*h*rgb);
     }
 
+//Установка цвета конкретного пикселя
+//Setting pixel color
+
     void setPixel(int x, int y, Color &c) {
         int tmp = y*rgb*w+x*rgb;
         source[tmp]=c.r;
         source[tmp+1]=c.g;
         source[tmp+2]=c.b;
     }
+
+//Взятие цвета конкретного пикселя
+//Getting pixel color
 
     Color getColor(int x, int y){
         int tmp = y*rgb*w+x*rgb;
@@ -45,6 +71,9 @@ public:
         return Color(r,g,b);
     }
 };
+
+//Рисование квадрата в координатах x,y; размерами w,h и цветом с
+//Drawing quard in coords x,y; width and height w, h and color c
 
 Image genQuard(Image image,int x,int y, int w,int h,Color c){
     for (int i=0;i<image.h;i++){
@@ -61,6 +90,9 @@ Image genQuard(Image image,int x,int y, int w,int h,Color c){
     }
     return image;
 }
+
+//Рисование окружности с центром в координатах x,y , радиуса r и цвета c
+//Drawing circle with center in coords x,y ; radius r and color c
 
 Image genCircle(Image image,int x,int y, int r,Color c){
     if (x>0 && y>0 && x < image.w && y< image.h){
@@ -82,6 +114,9 @@ Image genCircle(Image image,int x,int y, int r,Color c){
     return image;
 }
 
+//Заполнение изображения цветом c
+//Filling image with color с
+
 Image fillImage(Image image, Color c){
     int rgb=3;
     for (int i=0;i<image.h;i++){
@@ -97,6 +132,9 @@ Image fillImage(Image image, Color c){
     return image;
 }
 
+//Тестирование графических примитивов
+//Testing graphics
+
 int main()
 {
     int w = 640;
@@ -107,7 +145,7 @@ int main()
     Color white = Color(255,255,255);
     Color green = Color(0,255,0);
     img = fillImage(img,white);
-    img = genQuard(img,100,100,100,100,red);
+    img = genQuard(img,100,100,100,100,RED);
     img = genCircle(img,200,200,20,green);
 
     std::string filename = "image.png";
